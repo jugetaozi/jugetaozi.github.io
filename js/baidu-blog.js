260332d5f7d007cd23013ebd8a9eed8a6675ba42
WuDi <156343782@qq.com>
2017-09-02 18:55:45 +0800

    增加百度统计

---
+++ js/baidu-blog.js
@@ -0,0 +1,21 @@
+//百度统计数据
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ad7043a42dea926a79bdada91162942f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//百度链接提交
+(function () {
+  var bp = document.createElement('script');
+  var curProtocol = window.location.protocol.split(':')[0];
+  if (curProtocol === 'https') {
+      bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  } else {
+      bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(bp, s);
+})();
--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 $(function () {
-    //百度链接提交
-    (function () {
-        var bp = document.createElement('script');
-        var curProtocol = window.location.protocol.split(':')[0];
-        if (curProtocol === 'https') {
-            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-        } else {
-            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-        }
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(bp, s);
-    })();
-
     /* ---- particles.js config ---- */
     particlesJS("particles-after-filter", {
         "particles": {
